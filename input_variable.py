eea4518adc73fd457311fa3a89129887e514d84c
Muhammad Waqas Farid <Waqasfarid1994@gmail.com>
2022-01-28 22:11:07 +0500

    My first command

---
+++ input_variable.py
@@ -0,0 +1,50 @@
+# fruit_basket=input("What is your favourite fruit")
+# # print (fruit_basket)
+
+
+# # name =input("What is your name?  ")
+# # greeting = "Hello!" 
+# # print(greeting,name)
+
+
+
+# # name=input("What is your name? ")
+# # age=input("How old are you? ")
+# # greeting="Hello!"
+# # greeting2=('Your age is.... ')
+# # # print (greeting,name,greeting2,age )
+
+
+# a = 33
+# b = 200
+
+# if b > a:
+# print("b is greater then a")
+# elif a == b:
+# print("a is greater then b")
+
+
+# a =input()
+# b = input()
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+
+
+
+# i=1
+# while i<10:
+#     print(i)
+#     if i == 5:
+#      break 
+#     i+=1
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "cherry":
+    break
+  print(x)
